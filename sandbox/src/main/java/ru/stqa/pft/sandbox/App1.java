d876a8b74a793cf8c1bed6215bb429e36489a94f
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-04-24 13:32:35 +0200

    Method included to the class Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/App1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/App1.java
@@ -6,11 +6,9 @@ public class App1 {
     Point p1 = new Point(4, 7);
     Point p2 = new Point(2, 8);
 
-    System.out.println("Расстояние между точками равно " + distance(p1, p2));
+    System.out.println("Расстояние между точками равно " + p1.distance(p2));
 
   }
 
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-  }
+
 }
