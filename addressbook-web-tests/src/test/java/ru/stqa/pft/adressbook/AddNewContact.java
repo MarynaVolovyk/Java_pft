bb465690a277ad00ed1c830015e9bf357eca3c16
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-05-01 16:53:28 +0200

    test AddNewContact after refactoring

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/AddNewContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/AddNewContact.java
@@ -5,18 +5,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
 
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
 
 public class AddNewContact {
   FirefoxDriver wd;
@@ -26,38 +18,55 @@ public class AddNewContact {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
   @Test
   public void testAddNewContact() {
 
-    wd.findElement(By.linkText("add new")).click();
+    gotoAddNew();
+    fillNewContact("Nina", "Kilanina", "Lermontowska 20/9\nPoland\n30-300");
+    submitNewContact();
+    returnToHomePage();
+  }
+
+  private void fillNewContact(String nina, String kilanina, String s) {
+  }
+
+  private void returnToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitNewContact() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillNewContact(NewContact newContact) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Nina");
+    wd.findElement(By.name("firstname")).sendKeys(newContact.getName());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Kilanina");
+    wd.findElement(By.name("lastname")).sendKeys(newContact.getPassword());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Lermontowska 20/9");
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Lermontowska 20/9\nPoland");
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Lermontowska 20/9\nPoland\n30-300");
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    wd.findElement(By.linkText("home page")).click();
+    wd.findElement(By.name("address")).sendKeys(newContact.getAddress1());
+  }
+
+  private void gotoAddNew() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/NewContact.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.adressbook;
+
+public class NewContact {
+  private final String name;
+  private final String password;
+  private final String address1;
+
+  public NewContact(String name, String password, String address1) {
+    this.name = name;
+    this.password = password;
+    this.address1 = address1;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getAddress1() {
+    return address1;
+  }
+}
