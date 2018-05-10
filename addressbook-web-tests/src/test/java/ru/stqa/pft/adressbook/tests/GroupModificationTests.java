e114a6273d2e80e77029aa20cd9fdd8e14eadc21
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-05-10 10:56:38 +0200

    Check if group exists for GroupModification and GeoupDeletion Tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
@@ -9,9 +9,13 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification() {
 
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test5", null, "test7", null), false);
+    app.getGroupHelper().fillGroupForm(new GroupData("test5", null, "test7"));
     app.getGroupHelper().submitGroupModification();
   }
 }
