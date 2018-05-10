b5fa1ae707776fd1e7775227499b2ff9c654c3ac
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-05-10 11:49:44 +0200

    Check if Contact exists for ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
@@ -8,6 +8,10 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification() {
     app.getNavigationHelper().gotoHome();
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getNavigationHelper().gotoAddNew();
+      app.getContactHelper().createContact(new NewContact("katia", "annete", "Rydluwka 5, Krakow", "test1"), true);
+    }
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new NewContact("anna", null, null, null), false);
     app.getContactHelper().submitContactModification();
