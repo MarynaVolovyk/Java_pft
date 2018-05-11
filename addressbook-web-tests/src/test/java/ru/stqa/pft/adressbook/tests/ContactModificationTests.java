d69de4a98dd0b75b565ff8c7ade6f71153a72ace
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-05-11 16:21:56 +0200

    Method ContactCount added to the ContaactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.NewContact;
 
@@ -9,12 +10,17 @@ public class ContactModificationTests extends TestBase {
   public void testContactModification() {
     app.getNavigationHelper().gotoHome();
     if (!app.getContactHelper().isThereAContact()) {
+
       app.getNavigationHelper().gotoAddNew();
       app.getContactHelper().createContact(new NewContact("katia", "annete", "Rydluwka 5, Krakow", "test1"), true);
     }
+    int before = app.getContactHelper().getContactCount();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new NewContact("anna", null, null, null), false);
     app.getContactHelper().submitContactModification();
+    app.getNavigationHelper().gotoHome();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before);
 
   }
 
