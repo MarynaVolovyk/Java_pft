456896890e1e6b673cf052557463f13d1989c5a4
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-05-11 15:26:00 +0200

    Method CountGroup added to GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
@@ -13,9 +14,13 @@ public class GroupModificationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
 
     }
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test5", null, "test7"));
     app.getGroupHelper().submitGroupModification();
+    app.getNavigationHelper().gotoGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
   }
 }
