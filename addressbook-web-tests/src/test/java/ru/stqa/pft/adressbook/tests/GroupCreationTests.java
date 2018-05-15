9b79ada803835630bcef9129f336f07655658286
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-05-15 16:42:24 +0200

    Comparing sizes in GroupsTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -7,6 +7,8 @@ import org.testng.annotations.Test;
 
 import ru.stqa.pft.adressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
@@ -14,10 +16,11 @@ public class GroupCreationTests extends TestBase {
 
 
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before + 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+
+    Assert.assertEquals(after.size(), before.size() + 1);
   }
 }
 
