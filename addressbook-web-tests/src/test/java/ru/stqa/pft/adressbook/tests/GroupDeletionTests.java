fe505bc6af8853294ce7a1a812b6991049ff65e1
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-05-16 12:08:23 +0200

    Comparing collactions for GroupDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionTests.java
@@ -3,11 +3,9 @@ package ru.stqa.pft.adressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
-
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
-
 
   @Test
   public void testGroupDeletion() {
@@ -15,7 +13,6 @@ public class GroupDeletionTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-
     }
 
     List<GroupData> before = app.getGroupHelper().getGroupList();
@@ -24,6 +21,10 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+      Assert.assertEquals(before, after);
+
   }
 }
 
