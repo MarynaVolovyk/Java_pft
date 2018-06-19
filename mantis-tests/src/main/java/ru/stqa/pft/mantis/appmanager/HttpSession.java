b454e7f029712ffac5db6aa42fb6302d7bd39c18
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-06-19 15:18:45 +0200

    Adding LoginTests

--- mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -23,7 +23,7 @@ public class HttpSession {
     httpClient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
   }
   public boolean login(String username, String password) throws IOException {
-    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
+    HttpPost post = new HttpPost(app.getProperty("web.baseURL") + "/login.php");
     List<BasicNameValuePair> params = new ArrayList<>();
     params.add(new BasicNameValuePair("username", username));
     params.add(new BasicNameValuePair(("password"), password));
@@ -32,7 +32,7 @@ public class HttpSession {
     post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse response = httpClient.execute(post);
     String body = getTextFrom(response);
-    return body.contains(String.format("<span class=\"italic\">%s</span", username));
+    return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
   }
 
   private String getTextFrom(CloseableHttpResponse response) throws IOException {
@@ -43,9 +43,10 @@ public class HttpSession {
           }
   }
   public boolean isLoggedInAs(String username) throws IOException {
-    HttpGet get = new HttpGet((app.getProperty("web.baseUrl") + "/login.php"));
+    HttpGet get = new HttpGet((app.getProperty("web.baseURL") + "/login.php"));
     CloseableHttpResponse response = httpClient.execute(get);
     String body = getTextFrom(response);
-    return  body.contains(String.format("<span class=\"italic\">%s</span", username));
+//    return  body.contains(String.format("<span class=\"italic\">%s</span", username));
+    return  body.contains(String.format("<span class=\"user-info\">%s</span>", username));
   }
 }
