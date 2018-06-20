8550563dcbe3052f6a49798af2d4aaf4b6cac1bd
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-06-20 16:13:46 +0200

    Adding MailHelper

--- mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -9,13 +9,18 @@ public class RegistrationHelper extends HelperBase {
     super(app);
   }
 
-
-
   public void start(String username, String email) {
     String url = app.getProperty("web.baseURL") + "/signup_page.php";
     type(By.name("username"), username);
     type(By.name("email"), email);
     click(By.cssSelector("input[value='Signup']"));
 //    wd.get(url);
+  }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    click(By.cssSelector("input[value='Update User']"));
   }
 }
