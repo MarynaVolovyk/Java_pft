7a13d06699d4fc6f0ebd9764918354e6532e969e
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-06-20 14:20:39 +0200

    RegistrationHelper update

--- mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,21 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver wd;
+public class RegistrationHelper extends HelperBase {
 
   public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
-    wd = app.getDriver();
+    super(app);
   }
+
+
 
   public void start(String username, String email) {
     String url = app.getProperty("web.baseURL") + "/signup_page.php";
-    wd.get(url);
+    type(By.name("username"), username);
+    type(By.name("email"), email);
+    click(By.cssSelector("input[value='Signup']"));
+//    wd.get(url);
   }
 }
