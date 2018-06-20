fb2bcf730eab16eef8794da1061912908618a207
Maryna Volovyk <maryna.volovyk@order2cash.com>
2018-06-20 18:46:10 +0200

    Fix for MailHelper

--- mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.mantis.appmanager;
 import org.subethamail.wiser.Wiser;
 import org.subethamail.wiser.WiserMessage;
 import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
@@ -10,13 +12,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MailHelper {
-    private ApplicationManager app;
-    private final Wiser wiser;
+  private ApplicationManager app;
+  private final Wiser wiser;
 
   public MailHelper(ApplicationManager app) {
     this.app = app;
     wiser = new Wiser();
   }
+
   public List<MailMessage> waitForMail(int count, long timeout) throws MessagingException, IOException {
     long start = System.currentTimeMillis();
     while (System.currentTimeMillis() < start + timeout) {
@@ -32,18 +35,24 @@ public class MailHelper {
     throw new Error("No mail :(");
   }
 
-  public static MailMessage toModelMail(WiserMessage m) throws MessagingException {
+  public static MailMessage toModelMail(WiserMessage m) {
     try {
       MimeMessage mm = m.getMimeMessage();
       return new MailMessage(mm.getAllRecipients()[0].toString(), (String) mm.getContent());
+    } catch (MessagingException e) {
+      e.printStackTrace();
+      return null;
     } catch (IOException e) {
       e.printStackTrace();
       return null;
     }
   }
 
-  public void start() { wiser.start(); }
+  public void start() {
+    wiser.start();
+  }
 
-  public void stop() { wiser.stop(); }
-
+  public void stop() {
+    wiser.stop();
+  }
 }
